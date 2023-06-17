37fcbeabafdd6acf759d536b9b66dcb35d1231b7
Renan Joppert <renan@guaidor.digital>
2023-06-17 17:02:05 -0300

    Add ga4

--- front/src/main.js
+++ front/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(VueGtag, {
+    config: { id: 'G-BNLXHSPF7L' }
+})
 
 app.mount('#app')
